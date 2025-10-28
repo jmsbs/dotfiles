ecff74b1754deeb9eb042f1944bdc6429bce9590
João <jsilva@lumafintech.com>
2025-10-28 09:45:08 +0000

    First commit.

---
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      -- Show hidden and gitignored files by default
+      hidden = true,
+      no_ignore = true,
+      no_ignore_parent = true,
+    },
+  },
+}
